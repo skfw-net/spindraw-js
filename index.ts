2be0d91b6a94607d9d006dac75bccebb37057095
skulluglify <skulluglify@outlook.com>
2023-06-15 22:48:29 +0700

    feat/update retype private method

--- index.ts
+++ index.ts
@@ -46,47 +46,47 @@ export function ShuffleArray<T>(data: Array<T>): Array<T> {
 
 export class Draw {
   
-  #named: string
-  #value: float
+  private named: string
+  private value: float
 
   constructor(named: string, value: float) {
 
-    this.#named = named
-    this.#value = value
+    this.named = named
+    this.value = value
   }
 
-  get Gift(): string { return this.#named }
-  get Dist(): float { return this.#value }
+  get Gift(): string { return this.named }
+  get Dist(): float { return this.value }
 }
 
 export default class SpinDraw {
 
-  #roles: Map<string, float>
+  private roles: Map<string, float>
   
-  #z: float
+  private z: float
 
-  #named: Array<string>
-  #ranges: Array<float>
-  #k: float
+  private named: Array<string>
+  private ranges: Array<float>
+  private k: float
 
-  #nodups: Array<string>
+  private nodups: Array<string>
 
   constructor(roles: Map<string, float>, z: float = Number.MAX_SAFE_INTEGER) {
 
     let values: Array<float> = new Array;
 
     // init
-    this.#roles = roles;
-    this.#named = Array.from(this.#roles.keys())
+    this.roles = roles;
+    this.named = Array.from(this.roles.keys())
 
     // update
-    values = Array.from(this.#roles.values())
-    this.#ranges = this.RangeValues(...values) // pre-caches
-    this.#k = Sum(...values)
+    values = Array.from(this.roles.values())
+    this.ranges = this.RangeValues(...values) // pre-caches
+    this.k = Sum(...values)
     
     // once
-    this.#nodups = this.IndexesIntoNamed(this.IndexesOf(z))
-    this.#z = z
+    this.nodups = this.IndexesIntoNamed(this.IndexesOf(z))
+    this.z = z
   }
 
   RangeValues(...v: Array<float>): Array<float> {
@@ -116,13 +116,13 @@ export default class SpinDraw {
     let n: int
   
     v = 0
-    n = this.#ranges.length
+    n = this.ranges.length
 
     for (i = 0; i < n; i++) {
   
       a = v // before
-      //b = a + this.#ranges[i] // after
-      b = this.#ranges[i] // after
+      //b = a + this.ranges[i] // after
+      b = this.ranges[i] // after
       v = b // merging
   
       if (a <= q && q <= b) {
@@ -144,13 +144,13 @@ export default class SpinDraw {
     let n: int
   
     v = 0
-    n = this.#ranges.length
+    n = this.ranges.length
     
     for (i = 0; i < n; i++) {
   
       p = v
-      //v = p + this.#ranges[i]
-      v = this.#ranges[i]
+      //v = p + this.ranges[i]
+      v = this.ranges[i]
   
       // no sorted, check all ranges
       if (p <= q) { // 0.0 <= q, 0.1 <= q, 0.2 <= q, ...
@@ -169,13 +169,13 @@ export default class SpinDraw {
     let i: int
     let n: int
   
-    n = this.#named.length
+    n = this.named.length
   
     for (i of indexes) {
   
       if (i < n) {
   
-        temp.push(this.#named[i])
+        temp.push(this.named[i])
       }
     }
   
@@ -184,22 +184,22 @@ export default class SpinDraw {
 
   get Views(): Map<string, float> {
 
-    const entries = Array.from(this.#roles.entries())
-    this.#roles = new Map(entries.sort((a: [string, float], b: [string, float]) => a[1] - b[1]))
+    const entries = Array.from(this.roles.entries())
+    this.roles = new Map(entries.sort((a: [string, float], b: [string, float]) => a[1] - b[1]))
 
     this.Update()
 
-    const temp: Map<string, float> = new Map(this.#roles)
+    const temp: Map<string, float> = new Map(this.roles)
 
     let dist: float | undefined
     let percent: number
 
-    for (let named of this.#named) {
+    for (let named of this.named) {
 
       dist = temp.get(named)
       if (typeof dist !== "undefined") {
 
-        percent = (dist / this.#k) * 100
+        percent = (dist / this.k) * 100
         temp.set(named, percent)
       }
     }
@@ -211,17 +211,17 @@ export default class SpinDraw {
 
     let values: Array<float> = new Array
 
-    this.#named = Array.from(this.#roles.keys())
+    this.named = Array.from(this.roles.keys())
 
-    values = Array.from(this.#roles.values())
-    this.#ranges = this.RangeValues(...values) // pre-caches
-    this.#k = Sum(...values)
+    values = Array.from(this.roles.values())
+    this.ranges = this.RangeValues(...values) // pre-caches
+    this.k = Sum(...values)
   }
 
   RemoveAndUpdate(index: int): void {
   
     // remove
-    this.#roles.delete(this.#named[index])
+    this.roles.delete(this.named[index])
     
     // update
     this.Update()
@@ -229,8 +229,8 @@ export default class SpinDraw {
 
   Shuffle(): void {
 
-    const entries: Array<[string, float]> = Array.from(this.#roles.entries())
-    this.#roles = new Map(ShuffleArray(entries))
+    const entries: Array<[string, float]> = Array.from(this.roles.entries())
+    this.roles = new Map(ShuffleArray(entries))
     this.Update()
   }
 
@@ -240,16 +240,16 @@ export default class SpinDraw {
     let i: int
     let v: float
 
-    if (0 < this.#k) {
+    if (0 < this.k) {
 
       this.Shuffle()
     
-      v = RanRand(0, this.#k)
+      v = RanRand(0, this.k)
       i = this.IndexOf(v)
     
-      named = this.#named[i]
+      named = this.named[i]
   
-      if (this.#nodups.includes(named)) {
+      if (this.nodups.includes(named)) {
     
         this.RemoveAndUpdate(i)
       }
@@ -271,7 +271,7 @@ export default class SpinDraw {
     let n: float
     let v: float
 
-    if (0 < this.#k) {
+    if (0 < this.k) {
   
       n = 0
   
@@ -283,7 +283,7 @@ export default class SpinDraw {
   
         for (i = 0; i < n; i++) {
   
-          temp.push(this.#named.indexOf(named))
+          temp.push(this.named.indexOf(named))
         }
       }
   
@@ -292,12 +292,12 @@ export default class SpinDraw {
       h = temp.length - 1
       v = RanRand(0, h)
       i = Math.round(v)
-      v = (i / h) * this.#k
+      v = (i / h) * this.k
       i = temp[i]
 
-      named = this.#named[i]
+      named = this.named[i]
   
-      if (this.#nodups.includes(named)) {
+      if (this.nodups.includes(named)) {
   
         this.RemoveAndUpdate(i)
       }
@@ -310,16 +310,17 @@ export default class SpinDraw {
 
   get Roles(): Map<string, float> {
 
-    return this.#roles
+    // copy not assignable
+    return new Map(this.roles.entries())
   }
 
   set Roles(roles: Map<string, float>) {
 
     // set new roles
-    this.#roles = roles
+    this.roles = roles
     this.Update()
 
     // update nodups
-    this.#nodups = this.IndexesIntoNamed(this.IndexesOf(this.#z))
+    this.nodups = this.IndexesIntoNamed(this.IndexesOf(this.z))
   }
 }
